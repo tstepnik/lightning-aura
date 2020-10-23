13f4de9bf3efc95de05bad88f8b7141c9785e768
tstepnik <tomasz.stepnik77@gmail.com>
2020-10-23 13:46:11 +0200

    working event after remove old table

--- src/aura/AS_DivisionSearch_Results/AS_DivisionSearch_ResultsController.js
+++ src/aura/AS_DivisionSearch_Results/AS_DivisionSearch_ResultsController.js
@@ -1,14 +1,5 @@
 ({
-    init: function (cmp, event, helper) {
-        cmp.set('v.columns', [
-            {label: 'Account name', fieldName: 'name', type: 'text'},
-            {label: 'Country', fieldName: 'country', type: 'text'},
-            {label: 'Rating', fieldName: 'rating', type: 'text'},
-            {label: 'City', fieldName: 'city', type: 'text'},
-            {label: 'Active', fieldName: 'active', type: 'text'},
-            {label: 'Upsell Opportunity', fieldName: 'upsellOpportunity', type: 'text'},
-        ]);
-    },
+
 
     handleEvent: function(cmp, event, helper) {
         helper.handleEvent(cmp,event);
