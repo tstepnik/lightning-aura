d579c47c0a10bea9c9a1747208a23c7a21d792af
tstepnik <tomasz.stepnik77@gmail.com>
2020-10-27 00:42:02 +0100

    working edit/cancel

--- src/aura/AS_DivisionSearch_details/AS_DivisionSearch_detailsController.js
+++ src/aura/AS_DivisionSearch_details/AS_DivisionSearch_detailsController.js
@@ -1,9 +1,17 @@
 ({
-
+ doInit: function(component) {
+        component.set('v.isRecordEdited', false);
+    },
     handler: function(cmp,event,helper){
         console.log('account wrapper');
         console.log(event.getParam('accountWrapper'));
         let id = event.getParam('accountWrapper');
         cmp.set('v.wrapper',id);
-    }
+    },
+     showEditForm: function(cmp,event,helper){
+            cmp.set('v.isRecordEdited',true);
+        },
+        hideEditForm: function(cmp,event,helper){
+            cmp.set('v.isRecordEdited',false);
+        }
 })
